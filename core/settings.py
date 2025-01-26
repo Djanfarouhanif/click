b63d765385472b28fce1f799c3f36845e5d046c6
DJANFAROU <hanifdjanfarou8@gmail.com>
2025-01-26 00:10:47 +0000

    add corsheaders

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Url',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,8 +50,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200'
+]
 
 # Désactiver la protection CSRF pour les vues REST
 CSRF_TRUSTED_ORIGINS = ['http://localhost:8000']
